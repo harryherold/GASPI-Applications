0a9b912fd0d4f9fc92b58ff67ac382d15f06211d
Christian Herold <harryherold@googlemail.com>
2015-02-06 17:28:58 +0100

    implemented startup script for slurm

---
+++ startup-scripts/gaspi_slurm.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+function mysort { for i in ${node_array[@]}; do echo "$i"; done | sort -n; }
+
+program=$*
+
+export GASPI_SOCKET="$SLURM_PROCID"
+export GASPI_MFILE="$HOME/machinefile_$GASPI_SOCKET"
+
+NODES=`srun hostname`
+
+node_array=($NODES)
+
+sorted_array=( $(mysort) )
+
+echo "${sorted_array[@]}" | tr ' ' '\n' > $GASPI_MFILE
+
+export GASPI_MASTER=${sorted_array[0]}
+
+if [ 0 -eq "$SLURM_PROCID" ];then
+  export GASPI_TYPE="GASPI_MASTER"
+  echo "Master is $GASPI_MASTER"
+  echo "<<start application>>"
+else
+  export GASPI_TYPE="GASPI_WORKER"
+fi
+
+$program
+
+rm $GASPI_MFILE
